f4557beb720a20bc186ed944e4bde25c0eb2d751
Stefan Eng <eng_steff@hotmail.com>
2016-02-21 09:37:00 +0100

    Create and print a bunch of Positions.

--- eda132_ass02/src/eda132_ass02/Main.java
+++ eda132_ass02/src/eda132_ass02/Main.java
@@ -3,13 +3,12 @@ package eda132_ass02;
 public class Main {
 	public static void main(String[] args) {
 		Robot robot = new Robot();
-		System.out.println(robot.bump());
-		System.out.println(robot.bump());
-		System.out.println(robot.bump());
-		System.out.println(robot.bump());
-		System.out.println(robot.bump());
-		System.out.println(robot.bump());
-		System.out.println(robot.bump());
-		System.out.println(robot.bump());
+		int repetitions = 15;
+		for(int i = 0; i < repetitions; i++) {
+			int tempx = robot.rnd.nextInt(100);
+			int tempy = robot.rnd.nextInt(100);
+			Position pos = new Position(tempx, tempy);
+			System.out.println(pos);
+		}
 	}
 }
