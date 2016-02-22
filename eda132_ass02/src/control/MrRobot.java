33820ed738860414702c5d70c9eff9146f6cc95c
Stefan Eng <eng_steff@hotmail.com>
2016-02-22 18:28:41 +0100

    Robot got moves.

--- eda132_ass02/src/control/MrRobot.java
+++ eda132_ass02/src/control/MrRobot.java
@@ -1,5 +1,8 @@
 package control;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import model.Model;
@@ -9,43 +12,83 @@ public class MrRobot {
 	int x, y;
 	Random rnd;
 	String[] headings = {"N","W","S","E"};
+	//             N   W   S   E  
+	int xlist[] = {0, -1,  0,  0};
+	int ylist[] = {1,  0, -1,  1};
 	Model model;
+	
+	HashMap<String, int[]> direction_modifiers = new HashMap<String, int[]>();
 	
 	public MrRobot(int x, int y, String heading, Model model) {
 		this.x = x;
 		this.y = y;
 		this.heading = heading;
-		rnd = new Random();
+		this.rnd = new Random();
 		this.model = model;
+		
+		set_up_direction_modifiers();
 	}
 	
 	public void move() {
-		for(int i =0; i<100; i++) {
-			String heading = random_heading();
-			System.out.println(heading);
-			System.out.println(can_i_go(heading));
+
+		String new_heading = "";
+		ArrayList<String> possible_headings = get_possible_headings();
+
+		Boolean possible_to_keep_going = can_i_go(this.heading);
+		if (possible_to_keep_going) {
+			int percent = rnd.nextInt(100)+1;
+			if (percent <= 70) { 
+				new_heading = this.heading;
+			} else {
+				new_heading = pick_one(possible_headings);
+			}
+		} else {
+			new_heading = pick_one(possible_headings);
 		}
+		int[] modifiers = direction_modifiers.get(new_heading);
+		System.out.println("heading: "+new_heading);
+		System.out.println("modifiers: "+modifiers[0]+','+modifiers[1]);
+		this.x += modifiers[0];
+		this.y += modifiers[1];
+		System.out.println(this.x);
+		this.heading = new_heading;
 	}
 	
-	public String random_heading() {
-		return headings[rnd.nextInt(headings.length)];
+	private ArrayList<String> get_possible_headings() {
+		ArrayList<String> list = new ArrayList<String>();
+		for (String heading : headings) {
+			if (can_i_go(heading)) {
+				list.add(heading);
+			}
+		}
+		return list;
+	}
+
+	private String pick_one(ArrayList<String> list) {
+		return list.get(rnd.nextInt(list.size()));
 	}
 	
 	public Boolean can_i_go(String robot_heading) {
-		//Corr:        N   W   S   E  (Case switches are boring.)
-		int xlist[] = {0, -1,  0,  0};
-		int ylist[] = {1,  0, -1,  1};
-		int pos = 0;
-		for (String heading : headings) {
-			if(robot_heading.equals(heading)) {
-				break;
-			}
-			pos += 1;
-		}
-		int new_x = this.x + xlist[pos];
-		int new_y = this.y + ylist[pos];
+		int[] modifiers = direction_modifiers.get(robot_heading);
+		int new_x = this.x + modifiers[0];
+		int new_y = this.y + modifiers[1];
 		Boolean x_is_fine = new_x < model.getNumCols() && new_x > -1;
 		Boolean y_is_fine = new_y < model.getNumRows() && new_y > -1;
 		return x_is_fine && y_is_fine;
+	}
+	
+	private void set_up_direction_modifiers() {
+		int pos = 0;
+		for (String heading : headings) {
+			int[] int_array = new int[2];
+			int_array[0] = xlist[pos];
+			int_array[1] = ylist[pos];
+			this.direction_modifiers.put(heading, int_array);
+			pos += 1;
+		}
+	}
+	
+	public String toString() {
+		return String.format("(%d, %d)", this.x, this.y);
 	}
 }
